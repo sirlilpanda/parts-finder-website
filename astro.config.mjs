5495220fd5ffb7120085a9442e4314247d62fad8
sirlilpanda <sirlilpanda@gmail.com>
2024-11-20 23:22:44 +1300

    start up

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,24 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://parts-finder.sirlilpanda.studio',
   output: 'server',
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  vite: {
+    ssr: {
+      external: ['node:path'],
+    },
+  },
+
+  integrations: [auth()]
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,4 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
 	interface Locals extends Runtime {}
 }
+
